18c477438e9225b4b7e7d5111aa797e83b2df63b
ManishJoc14 <manishjoc14@gmail.com>
2024-04-19 19:33:32 +0545

    Next auth implemented

--- app/page.tsx
+++ app/page.tsx
@@ -10,24 +10,24 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+      <div className="mt-4 flex grow flex-col gap-4 lg:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 lg:w-2/5 md:px-20">
           <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
           <p
             className={`${lusitana.className} text-1xl text-gray-800 md:text-2xl md:leading-normal`}
           >
             <strong>Welcome to Acme.</strong> This is the Nextjs Dashboard {' '}
-            it has all features of NEXT like data fetching from database(from postgres hosted on vercel) 
+            it has all features of NEXT like Next authentication, data fetching from database(from postgres hosted on vercel) 
             SSR, streaming, pagination, partial pre-rendering and many more.  
           </p>
           <Link
-            href="/dashboard"
+            href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Open Dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Login</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <div className="flex items-center justify-center p-6 lg:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
